db1d28ae0893e98dcb8b258d338501aac317ee42
Elena <ebe2017@yandex.ru>
2023-05-08 23:18:03 +0400

    project is started

---
+++ src/js/main.js
@@ -0,0 +1,13 @@
+
+const hamburger = document.querySelector('.header__toggle')
+
+if (hamburger) {
+    menu = document.querySelector('.main-nav');
+    phone = document.querySelector('.header__phone');
+    hamburger.addEventListener("click", function(e){
+        document.body.classList.toggle('body--lock');
+        hamburger.classList.toggle('header__toggle--active');
+        menu.classList.toggle('main-nav--active');
+        phone.classList.toggle('header__phone--active')
+    });
+};
