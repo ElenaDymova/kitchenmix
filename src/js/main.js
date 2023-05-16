12fd841e493907e285dea94411c70198d46e23bf
Elena <ebe2017@yandex.ru>
2023-05-16 17:50:06 +0400

    was added block with popular models and popups for this block

--- src/js/main.js
+++ src/js/main.js
@@ -13,5 +13,62 @@ if (hamburger) {
     });
 };
 
-//cards
+//modal
+    function bindModal(triggerSelector, popupSelector, modalSelector, closeSelector) { //привязка мод окна к опред тригеру (тригер-кнопка модальное окно кнопка закрытия)
+        const trigger = document.querySelectorAll(triggerSelector),
+              popup = document.querySelector(popupSelector),
+              modal = document.querySelector(modalSelector),
+              close = document.querySelector(closeSelector);
+        
+        trigger.forEach(item => {
+            item.addEventListener('click', (e) =>{ //событие по клику
+                if (e.target) { //удостоверяемся что существует элемент на кот кликнули
+                    e.preventDefault(); //отменили стандартное поведение
+                }
+                modal.style.display = "block";
+                popup.style.display = "block";
+                document.body.style.overflow = "hidden"; //фиксируем фон
+            });
+        });
+
+        close.addEventListener('click', () => {
+            modal.style.display = "none";
+            popup.style.display = "none";
+            document.body.style.overflow = ""; 
+        });
+
+        popup.addEventListener('click', (e) => { //закрытие формы по клику на область вокруг формы
+            if (e.target === popup) {
+                modal.style.display = "none";
+                popup.style.display = "none";
+                document.body.style.overflow = ""; 
+            }
+        });
+    }
+
+    bindModal('.card__button', '.popup', '.popup-catalog', '.popup-catalog .popup__close');
+
+    //buttons in modal description and characteristic
+    buttonDescr = document.querySelector('.popup-catalog__button--description'),
+    buttonCharacter = document.querySelector('.popup-catalog__button--characteristic'),
+    description = document.querySelector('.popup-catalog__description'),
+    characteristic = document.querySelector('.popup-catalog__characteristic');
+
+
+    buttonDescr.addEventListener('click', () => {
+        buttonCharacter.classList.remove('popup-catalog__button--description--active');
+        buttonDescr.classList.add('popup-catalog__button--description--active');
+        description.style.display = "block";
+        characteristic.style.display = "none";
+    });
+
+    buttonCharacter.addEventListener('click', () => {
+        buttonCharacter.classList.add('popup-catalog__button--description--active');
+        buttonDescr.classList.remove('popup-catalog__button--description--active');
+        description.style.display = "none";
+        characteristic.style.display = "block";
+    });
+
+
+
 
