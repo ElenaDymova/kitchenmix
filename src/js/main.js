8d908cb57dd06251494c262343b625bef1ac2a49
Elena <ebe2017@yandex.ru>
2023-06-08 19:54:23 +0400

    small changes

--- src/js/main.js
+++ src/js/main.js
@@ -4,7 +4,7 @@ const hamburger = document.querySelector('.header__toggle');
 
 if (hamburger) {
     menu = document.querySelector('.main-nav');
-    phone = document.querySelector('.header__phone');
+
     hamburger.addEventListener("click", function(e){
         document.body.classList.toggle('body--lock');
         hamburger.classList.toggle('header__toggle--active');
